f7301b593cffc39fe8f5b90030ba044c58b64c2e
guokai <guokaigdg@gmail.com>
2020-10-12 01:47:57 +0800

    209. 长度最小的子数组

--- 🚴刷题/ac/209. 长度最小的子数组.js
+++ 🚴刷题/ac/209. 长度最小的子数组.js
@@ -1,3 +1,5 @@
+//滑动数组
+//1
 var minSubArrayLen = function (s, nums) {
   if (Math.max(...nums) >= s) {
     return 1;
@@ -16,4 +18,28 @@ var minSubArrayLen = function (s, nums) {
   }
   return min === nums.length + 1 ? 0 : min;
 };
+console.log(minSubArrayLen(7, [2, 3, 1, 2, 4, 3]));
+// 2
+var minSubArrayLen = function (s, nums) {
+  if (Math.max(...nums) >= s) return 1;
+  if (nums.length === 0) return 0;
+  let len = nums.length;
+  let minLen = len + 1;
+  let newNums = [];
+  let sum = 0;
+  const add = (array) => {
+    return array.reduce((a, b) => a + b);
+  };
+  for (let i = 0; i < len; i++) {
+    newNums.push(nums[i]);
+    sum = add(newNums);
+    while (sum >= s) {
+      minLen = newNums.length < minLen ? newNums.length : minLen;
+      newNums.shift();
+      sum = add(newNums);
+    }
+  }
+  return minLen === len + 1 ? 0 : minLen;
+};
+
 console.log(minSubArrayLen(7, [2, 3, 1, 2, 4, 3]));
